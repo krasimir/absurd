1252c0cd2050f96f142c45acb1bddf7e231ee68b
Krasimir Tsonev <krasimir@outset.ws>
2013-08-29 11:33:49 +0300

    support of pseudo classes

---
+++ tests/pseudo-classes.spec.js
@@ -0,0 +1,29 @@
+describe("Nested selectors", function() {
+
+	var Absurd = require("../index.js");
+
+	it("should use nesting", function(done) {
+		Absurd(function(A) {
+			A.add({
+				a: {
+					'text-decoration': 'none',
+					'color': '#000',
+					':hover': {
+						'text-decoration': 'underline',
+						'color': '#999'
+					},
+					':before': {
+						content: '"> "'
+					}
+				}
+			});
+		}).compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toContain('a:hover {');
+			expect(css).toContain('a:before {');
+			done();
+		});
+	});
+
+});
