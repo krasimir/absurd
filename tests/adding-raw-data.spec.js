3a55251e64e4f64d74a63e3f69782208bd811aa0
Krasimir Tsonev <krasimir@outset.ws>
2013-10-06 10:53:24 +0300

    Sending raw data to the final compiled CSS

---
+++ tests/adding-raw-data.spec.js
@@ -0,0 +1,31 @@
+describe("Adding raw data", function() {
+
+	var Absurd = require('../index.js'),
+		comment = "AbsurdJS is awesome!";
+
+	it("should send raw data", function(done) {
+		Absurd(function(api) {
+			api
+			.add({
+				body: {
+					marginTop: "20px",
+					p: {
+						fontSize: "5px"
+					}
+				}
+			})
+			.raw('/* ' + comment + ' */')
+			.add({
+				a: {
+					paddingTop: "20px"
+				}
+			});
+		}).compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe("body {\n  margin-top: 20px;\n}\nbody p {\n  font-size: 5px;\n}\n/* " + comment + " */\na {\n  padding-top: 20px;\n}\n");
+			done();
+		});		
+	});
+
+});
