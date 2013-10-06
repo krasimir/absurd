ee1a3a4ba4082a598bae0ef0bbeb6da601ef43de
Krasimir Tsonev <krasimir@outset.ws>
2013-10-07 02:01:04 +0300

    Convering @charset

---
+++ tests/cases/charset/code.js
@@ -0,0 +1,8 @@
+module.exports = function(api) {	
+	api.add({
+		body: {
+			width: "100%"
+		},
+		"@charset": "UTF-8"
+	});
+}
---
+++ tests/cases/charset/test.spec.js
@@ -0,0 +1,32 @@
+describe("Test case (charset)", function() {
+
+	var Absurd = require('../../../index.js');
+
+	it("charset / js", function(done) {
+		Absurd(__dirname + '/code.js').compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe('body {\n  width: 100%;\n}\n@charset: "UTF-8";\n');
+			done();
+		});
+	});
+
+	it("charset / json", function(done) {
+		Absurd(__dirname + '/code.json').compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe('body {\n  width: 100%;\n}\np {\n  margin-top: 20px;\n}\n@charset: "UTF-8";\n');
+			done();
+		});
+	});
+
+	it("charset / css", function(done) {
+		Absurd(__dirname + '/code.css').compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe('body {\n  width: 100%;\n}\np {\n  margin-top: 20px;\n}\n@charset: "UTF-8";\na {\n  font-size: 10px;\n}\n');
+			done();
+		});
+	});
+
+});
