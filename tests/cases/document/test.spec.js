b5f81e96fe2364eb302d7855b1fd030e855841d1
Krasimir Tsonev <krasimir@outset.ws>
2013-10-07 17:12:29 +0300

    Refactoring CSS handling + handle document

---
+++ tests/cases/document/test.spec.js
@@ -0,0 +1,14 @@
+describe("Test case (Document)", function() {
+
+	var Absurd = require('../../../index.js');
+
+	it("Document / css", function(done) {
+		Absurd(__dirname + '/code.css').compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe('@-moz-document url-prefix() {\n.ui-select .ui-btn select {\n  opacity: .0001;\n}\n}\n');
+			done();
+		});
+	});
+
+});
