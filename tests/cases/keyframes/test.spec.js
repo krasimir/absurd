0dd510f2cf8d17b50b2510f09d111a760b0f6604
Krasimir Tsonev <krasimir@outset.ws>
2013-10-08 22:51:29 +0300

    Finishing keyframes covering + raw content bug fix

--- tests/cases/keyframes/test.spec.js
+++ tests/cases/keyframes/test.spec.js
@@ -2,11 +2,29 @@ describe("Test case (keyframes)", function() {
 
 	var Absurd = require('../../../index.js');
 
-	it("keyframes / js", function(done) {
+	xit("keyframes / js", function(done) {
 		Absurd(__dirname + '/code.js').compile(function(err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
 			expect(css).toBe('@keyframes fade {\n0% {\n  opacity: 0;\n}\n100% {\n  opacity: 1;\n}\n}\n');
+			done();
+		});
+	});
+
+	xit("keyframes / json", function(done) {
+		Absurd(__dirname + '/code.json').compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe('@keyframes fade {\n0% {\n  opacity: 0;\n}\n100% {\n  opacity: 1;\n}\n}\n');
+			done();
+		});
+	});
+
+	it("keyframes / css", function(done) {
+		Absurd(__dirname + '/code.css').compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe('@keyframes fade {\n0% {\n  opacity: 0;\n}\n100% {\n  opacity: 1;\n}\n}\n@keyframes move {\nfrom {\n  top: 10px;\n  margin: 20px 0 0 0;\n}\nto {\n  top: 12px;\n  margin: 40px 0 0 0;\n}\n}\n');
 			done();
 		});
 	});
