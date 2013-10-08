9381c22839c3212b6029a0b66b2c7971f23e83f3
Krasimir Tsonev <krasimir@outset.ws>
2013-10-08 16:55:28 +0300

    Covering keyframes (WIP)

--- tests/cases/keyframes/test.spec.js
+++ tests/cases/keyframes/test.spec.js
@@ -4,10 +4,9 @@ describe("Test case (keyframes)", function() {
 
 	it("keyframes / js", function(done) {
 		Absurd(__dirname + '/code.js').compile(function(err, css) {
-			console.log(css);
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe('');
+			expect(css).toBe('@keyframes fade {\n0% {\n  opacity: 0;\n}\n100% {\n  opacity: 1;\n}\n}\n');
 			done();
 		});
 	});
