c31fad5ea9c7dc0fb4509f230f600f9d4a269b56
Krasimir Tsonev <krasimir@outset.ws>
2013-10-10 01:28:53 +0300

    Preparing example page

---
+++ client-side/example/css/styles.js
@@ -0,0 +1,20 @@
+module.exports = function(api) {
+	api.add({
+		"html, body": {
+			margin: 0,
+			padding: 0,
+			width: "100%",
+			height: "100%",
+			fontFamily: "Georgia",
+			fontSize: "20px",
+			lineHeight: "24px"
+		},
+		h1: {
+			textAlign: "center",
+			fontWeight: "normal",
+			fontSize: "60px",
+			padding: "60px",
+			margin: "0"
+		}
+	})
+}
