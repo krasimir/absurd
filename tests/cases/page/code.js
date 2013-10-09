b89049fe8dca3a95b111bd47ac9c9f8ed285a87e
Krasimir Tsonev <krasimir@outset.ws>
2013-10-09 15:22:39 +0300

    Covering @page

---
+++ tests/cases/page/code.js
@@ -0,0 +1,10 @@
+module.exports = function(api) {
+	api.add({
+		"@page toc, index:blank": {
+			color: "#000"
+		},
+		"@page": {
+			fontSize: "12px"
+		}
+	});
+}
--- tests/cases/page/test.spec.js
+++ tests/cases/page/test.spec.js
@@ -1,30 +1,30 @@
-describe("Test case (namespace)", function() {
+describe("Test case (page)", function() {
 
 	var Absurd = require('../../../index.js');
 
-	it("namespace / js", function(done) {
+	it("page / js", function(done) {
 		Absurd(__dirname + '/code.js').compile(function(err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe('@namespace: "http://www.w3.org/1999/xhtml";\n');
+			expect(css).toBe('@page toc, index:blank {\n  color: #000;\n}\n@page {\n  font-size: 12px;\n}\n');
 			done();
 		});
 	});
 
-	it("namespace / json", function(done) {
+	it("page / json", function(done) {
 		Absurd(__dirname + '/code.json').compile(function(err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe('@namespace: "http://www.w3.org/1999/xhtml";\n');
+			expect(css).toBe('@page toc, index:blank {\n  color: #000;\n}\n@page {\n  font-size: 12px;\n}\n');
 			done();
 		});
 	});
 
-	it("namespace / css", function(done) {
+	it("page / css", function(done) {
 		Absurd(__dirname + '/code.css').compile(function(err, css) {
 			expect(err).toBe(null);
 			expect(css).toBeDefined();
-			expect(css).toBe('@namespace: "http://www.w3.org/1999/xhtml";\n');
+			expect(css).toBe('@page toc, index:blank {\n  color: #000;\n}\n@page {\n  font-size: 12px;\n}\n');
 			done();
 		});
 	});
