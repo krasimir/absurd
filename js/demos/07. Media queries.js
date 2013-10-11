e31b0eb99ef48dfe5dd9aca5a65285b7ab08e216
Krasimir Tsonev <krasimir@outset.ws>
2013-10-11 15:43:08 +0300

    Adding demos

---
+++ js/demos/07. Media queries.js
@@ -0,0 +1,15 @@
+api.add({
+	body: {
+		".wrapper": {
+			width: "940px",
+			"@media all (min-width: 550px)": {
+				width: "400px"
+			}
+		},
+		p: {
+			"@media all (min-width: 550px)": {
+				fontSize: "20px"
+			}	
+		}
+	}
+});
---
+++ js/demos/08. Sending raw data.js
@@ -0,0 +1,16 @@
+api
+.add({
+	body: {
+		".wrapper": {
+			width: "940px",
+			fontSize: "20px"
+		}
+	}
+})
+.raw("/* content */")
+.add({
+	".content": {
+		fontSize: "18px"
+	}
+})
+.raw("/* end of styles */");
