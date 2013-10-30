67afb02d03566b28e739cd3db5d92e51736604eb
litusapp <gundead222@ubuntu.ubuntu-domain>
2013-10-30 18:21:11 +0800

    modified:   lib/api/import.js modified:   package.json new file:   tests/common/using-yaml.spec.js new file:   tests/data/styles.yaml Now using js-yaml library instead of yamljs

---
+++ tests/common/using-yaml.spec.js
@@ -0,0 +1,25 @@
+describe("Import YAML instead of .js file", function() {
+
+	var Absurd = require('../../index.js');
+
+	it("importing yaml (api usage)", function(done) {
+		Absurd(function(api) {
+			api.import(__dirname + "/../data/styles.yaml");
+		}).compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe("body {\n  margin: 0;\n  padding: 0;\n  font-size: 1em;\n}\nbody p {\n  line-height: 30px;\n}\n");
+			done();
+		});	
+	});
+
+	it("importing yaml", function(done) {
+		Absurd(__dirname + "/../data/styles.yaml").compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			expect(css).toBe("body {\n  margin: 0;\n  padding: 0;\n  font-size: 1em;\n}\nbody p {\n  line-height: 30px;\n}\n");
+			done();
+		});	
+	});
+
+});
