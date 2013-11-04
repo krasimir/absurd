07ee9ad6e5eca2a348d2500b5013e8a476fdde53
Krasimir Tsonev <krasimir@outset.ws>
2013-11-04 16:40:20 +0200

    Working on HTML preprocessing

--- tests/metamorphosis/morph.to.html.preprocessor.spec.js
+++ tests/metamorphosis/morph.to.html.preprocessor.spec.js
@@ -4,7 +4,61 @@ describe("Metamorphosis (to html preprocessor)", function() {
 
 	api.morph("html");
 
-	xit("should accept and compile html", function(done) {
+	it("should compile raw content", function(done) {
+		api.add({
+			_raw: '<html></html>'
+		}).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<html></html>');
+			done();
+		});
+	});
+
+	it("should compile an empty tag", function(done) {
+		api.add({
+			body: {}
+		}).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<body/>');
+			done();
+		});
+	});
+
+	it("should compile an empty tag with attributes", function(done) {
+		api.add({
+			body: {
+				_attrs: {
+					class: "black",
+					onload: "javascript: alert('hello');"
+				}
+			}
+		}).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<body class="black" onload="javascript: alert(\'hello\');"/>');
+			done();
+		});
+	});
+
+	it("should compile nested tags", function(done) {
+		api.add({
+			html: {
+				head: {
+					title: "title"
+				},
+				body: {}
+			}
+		}).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<html>\n<head>\n<title>title</title>\n</head>\n<body/>\n</html>');
+			done();
+		});
+	});
+
+	it("complex html", function(done) {
 		api.add({
 			_raw:'<!DOCTYPE html>',
 			html: {
@@ -18,26 +72,26 @@ describe("Metamorphosis (to html preprocessor)", function() {
 					}
 				},
 				body: {
+					_attrs: { class: "home-page" },
 					section: {
 						h1: "that's html page"
 					}
 				}
 			}
 		}).compile(function(err, html) {
-			console.log(html);
 			expect(err).toBe(null);
 			expect(html).toBeDefined();
 			expect(html).toBe('<!DOCTYPE html>\n\
 <html>\n\
-  <head>\n\
-    <title>html page</title>\n\
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n\
-  </head>\n\
-  <body>\n\
-    <section>\n\
-      <h1>that\'s html page</h1>\n\
-    </section>\n\
-  </body>\n\
+<head>\n\
+<title>html page</title>\n\
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\n\
+</head>\n\
+<body class="home-page">\n\
+<section>\n\
+<h1>that\'s html page</h1>\n\
+</section>\n\
+</body>\n\
 </html>');
 			done();
 		});
