2d162a49ce3b15db70f0e105469147b2287fa360
Krasimir Tsonev <krasimir@outset.ws>
2013-11-06 17:09:37 +0200

    Improving HTML preprocessing

--- client-side/build/absurd.js
+++ client-side/build/absurd.js
@@ -709,6 +709,12 @@ var process = function(tagName, obj) {
 		childs += value;
 	}
 
+	var tagAnalized = analizeProperty(tagName);
+	tagName = tagAnalized.tag;
+	if(tagAnalized.attrs != "") {
+		attrs += " " + tagAnalized.attrs;
+	}
+
 	// process directives
 	for(var directiveName in obj) {
 		var value = obj[directiveName];
@@ -743,9 +749,10 @@ var process = function(tagName, obj) {
 		}
 	}
 
-	for(var name in obj) {
-		var value = obj[name];
+	for(var prop in obj) {
+		var value = obj[prop];
 		if(value !== false) {
+			var name = prop;
 			switch(typeof value) {
 				case "string": addToChilds(process(name, value)); break;
 				case "object": 
@@ -782,6 +789,78 @@ var packTag = function(tagName, attrs, childs) {
 		html += '<' + transformUppercase(tagName) + attrs + '/>';
 	}
 	return html;
+}
+
+var analizeProperty = function(prop) {
+	var res = { 
+			tag: '',
+			attrs: ''
+		},
+		numOfChars = prop.length,
+		tagName = "",
+		className = "", readingClass = false, classes = [],
+		idName = "", readingId = false, ids = [],
+		attributes = "", readingAttributes = false;
+
+	for(var i=0; c=prop[i]; i++) {
+		if(c === "." && !readingClass) {
+			readingClass = true;
+		} else if(readingClass) {
+			if(c != "." && c != "#" && c != "[") {
+				className += c;
+			} else {
+				classes.push(className);
+				readingClass = false;
+				className = "";
+				i -= 1;
+			}
+		} else if(c === "#" && !readingId) {
+			readingId = true;
+		} else if(readingId) {
+			if(c != "." && c != "#" && c != "[") {
+				idName += c;
+			} else {
+				readingId = false;
+				i -= 1;
+			}
+		} else if(c === "[" && !readingAttributes) {
+			readingAttributes = true;
+		} else if(readingAttributes) {
+			if(c != "." && c != "#" && c != "]") {
+				attributes += c;
+			} else {
+				readingAttributes = false;
+				i -= 1;
+			}
+		} else if(c != "." && c != "#" && c != "[" && c != "]") {
+			res.tag += c;
+		}
+	}
+
+	// if ends with a class
+	if(className != "") classes.push(className);
+
+	// collecting classes
+	var clsStr = '';
+	for(var i=0; cls=classes[i]; i++) {
+		clsStr += clsStr === "" ? cls : " " + cls;
+	}
+	res.attrs += clsStr != "" ? 'class="' + clsStr + '"' : '';
+
+	// if ends on id
+	if(idName != "") {
+		res.attrs += (res.attrs != "" ? " " : "") + 'id="' + idName + '"';
+	}
+
+	// if div tag name is skipped
+	if(res.tag === "" && res.attrs != "") res.tag = "div";
+
+	// collecting attributes
+	if(attributes != "") {
+		res.attrs += (res.attrs != "" ? " " : "") + attributes;
+	}
+
+	return res;
 }
 
 lib.processors.html.HTML = function() {
---
+++ tests/metamorphosis/html/morph.html.zen.spec.js
@@ -0,0 +1,91 @@
+describe("Metamorphosis (to html preprocessor)", function() {
+
+	var api = require('../../../index.js')();
+
+	api.morph("html");
+
+	it("should use classes", function(done) {
+		var tags = {
+			"div.content": {
+				p: "text"
+			}
+		}
+		api.add(tags).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<div class="content">\n<p>\ntext\n</p>\n</div>');
+			done();
+		});
+	});
+
+	it("should use two classes", function(done) {
+		var tags = {
+			"div.content.left": {
+				p: "text"
+			}
+		}
+		api.add(tags).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<div class="content left">\n<p>\ntext\n</p>\n</div>');
+			done();
+		});
+	});
+
+	it("should use id", function(done) {
+		var tags = {
+			"div#content": {
+				p: "text"
+			}
+		}
+		api.add(tags).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<div id="content">\n<p>\ntext\n</p>\n</div>');
+			done();
+		});
+	});
+
+	it("should use id and class together", function(done) {
+		var tags = {
+			"div.content#home.left": {
+				p: "text"
+			}
+		}
+		api.add(tags).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<div class="content left" id="home">\n<p>\ntext\n</p>\n</div>');
+			done();
+		});
+	});
+
+	it("should use without tag name", function(done) {
+		var tags = {
+			".content#home.left": {
+				p: "text"
+			}
+		}
+		api.add(tags).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<div class="content left" id="home">\n<p>\ntext\n</p>\n</div>');
+			done();
+		});
+	});
+
+	it("should pass attributes", function(done) {
+		var tags = {
+			'.content[data-behaviour="clickable" title="test" style="position: absolute; top: 20px; left: 30px;"]#home': {
+				'img[alt="that\'s my image" some__data="1"]': {}
+			}
+		}
+		api.add(tags).compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<div class="content" id="home" data-behaviour="clickable" title="test" style="position: absolute; top: 20px; left: 30px;">\n<img alt="that\'s my image" some__data="1"/>\n</div>');
+			done();
+		});
+	});
+
+});
