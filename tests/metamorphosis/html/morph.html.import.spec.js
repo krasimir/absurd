2c92987ddee58117f7a6d379ba7a12ce82000295
OpenShift guest <emmangamboa@gmail.com>
2013-11-08 21:24:30 -0500

    Added yaml import to html test

--- tests/metamorphosis/html/morph.html.import.spec.js
+++ tests/metamorphosis/html/morph.html.import.spec.js
@@ -24,5 +24,26 @@ text here\n\
 			done();
 		});
 	});
+	
+	it("should use the import method", function(done) {
+		api.import(__dirname + "/../../data/html.yml").compile(function(err, html) {
+			expect(err).toBe(null);
+			expect(html).toBeDefined();
+			expect(html).toBe('<!DOCTYPE html>\n\
+<head>\n\
+<title>\n\
+AbsurdJS is awesome\n\
+</title>\n\
+</head>\n\
+<body>\n\
+<div class="container">\n\
+<p>\n\
+text here\n\
+</p>\n\
+</div>\n\
+</body>');
+			done();
+		});
+	});
 
 });
