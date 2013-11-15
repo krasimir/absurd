5487e26f436c3c4292a81d642322eb207f8c3239
Krasimir Tsonev <krasimir@outset.ws>
2013-11-15 13:33:02 +0200

    Improving ampersand support + test for CLI version

---
+++ tests/common/cli.spec.js
@@ -0,0 +1,67 @@
+describe("API(add)", function() {
+
+	var api = require('../../index.js')();
+	var exec = require('child_process').exec;
+
+	var shell = function(cmd, callback) {
+		cmd = "node " + __dirname + "/../../index.js " + cmd;
+		exec(cmd, function (error, stdout, stderr) {
+		    // console.log('stdout: "' + stdout + '"');
+		    // console.log('stderr: "' + stderr + '"');
+		    callback(stderr, stdout);
+		}, { encoding: 'utf8'});
+	}
+
+	it("should use -s (source) option", function(done) {
+		shell("-s ./tests/data/styles.json", function(err, res) {
+			expect(err).toBe("");
+			expect(res).toBe("body {\n\
+  margin: 0;\n\
+  padding: 0;\n\
+  font-size: 1em;\n\
+}\n\
+body p {\n\
+  line-height: 30px;\n\
+}\n\n\
+")
+			done();
+		});
+	});
+
+	it("should use -m (minify) option", function(done) {
+		shell("-s ./tests/data/styles.json -m true", function(err, res) {
+			expect(err).toBe("");
+			expect(res).toBe("body{margin:0;padding:0;font-size:1em}body p{line-height:30px}\n");
+			done();
+		});
+	});
+
+	it("should use -m (minify) option", function(done) {
+		shell("-s ./tests/data/html.json -t html", function(err, res) {
+			expect(err).toBe("");
+			expect(res).toBe("<!DOCTYPE html>\n\
+<head>\n\
+<title>\n\
+AbsurdJS is awesome\n\
+</title>\n\
+</head>\n\
+<body>\n\
+<div class=\"container\">\n\
+<p>\n\
+text here\n\
+</p>\n\
+</div>\n\
+</body>\n");
+			done();
+		});
+	});
+
+	it("should use -k (keep camel case) option", function(done) {
+		shell("-s ./tests/data/css/keepcamelcase.json -k true", function(err, res) {
+			expect(err).toBe("");
+			expect(res).toBe("body {\n  lineHeight: 20px;\n}\n\n");
+			done();
+		});
+	});
+
+});
