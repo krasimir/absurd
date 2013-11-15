7192c177cab4b3808bff2b1249a91c4c835e7143
OpenShift guest <emmangamboa@gmail.com>
2013-11-14 19:06:21 -0500

    Ampersand operator on any position in the nested selector Added new test in use of Ampersand operator modified:   lib/api/add.js modified:   tests/common/use-ampersand-operator.spec.js

--- tests/common/use-ampersand-operator.spec.js
+++ tests/common/use-ampersand-operator.spec.js
@@ -16,5 +16,23 @@ describe("Use ampersand operator", function() {
 			done();
 		})
 	});
+	
+	it("should replace all ampersand with parent selector", function(done) {
+		api.add({
+			a: {
+				color: 'red',
+				':hover': { color: 'blue' },
+				'&.fancy': { color: 'green' },
+				'.ie6 &:hover': { color: 'orange' },
+				'.ie6 &.fancy': { color: 'yellow' }
+			}
+		}).compile(function(err, css) {
+			expect(err).toBe(null);
+			expect(css).toBeDefined();
+			console.log(css);
+			expect(css).toBe("a {\n  color: red;\n}\na:hover {\n  color: blue;\n}\na.fancy {\n  color: green;\n}\n.ie6 a:hover {\n  color: orange;\n}\n.ie6 a.fancy {\n  color: yellow;\n}\n");
+			done();
+		})
+	});
 
 });
