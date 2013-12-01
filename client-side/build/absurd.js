1f771a494e3a103c0a3c4c58f537aeeec550f7c6
Krasimir Tsonev <krasimir@outset.ws>
2013-12-01 16:11:48 +0200

    Improving template engine

--- client-side/build/absurd.js
+++ client-side/build/absurd.js
@@ -1050,24 +1050,19 @@ lib.processors.html.helpers.PropAnalyzer = function(prop) {
 	return res;
 }
 lib.processors.html.helpers.TemplateEngine = function(html, options) {
-	var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
-		matches = [], code = 'var r=[];\n', cursor = 0;
-	var compile = function(code) {
-		return (new Function("data", code.replace(/[\r\t\n]/g, ''))).apply(options);
-	}
+	var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0;
 	var add = function(line, js) {
 		js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
 			(code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
 		return add;
 	}
-	while(match = re.exec(html)) matches.push(match);
-	for(var i=0; i<matches, m=matches[i]; i++) {
-		add(html.slice(cursor, m.index))(m[1], true);
-		cursor = m.index + m[0].length;
+	while(match = re.exec(html)) {
+		add(html.slice(cursor, match.index))(match[1], true);
+		cursor = match.index + match[0].length;
 	}
 	add(html.substr(cursor, html.length - cursor));
 	code += 'return r.join("");';
-	return compile(code);
+	return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
 };
 return client();
 })(window);
