98d0aae8ee7ec943ec6360203af6e44e8cafcf62
Krasimir Tsonev <krasimir@outset.ws>
2013-12-15 16:36:59 +0200

    Updating version

--- js/absurd.js
+++ js/absurd.js
@@ -1,4 +1,4 @@
-/* version: 0.1.16 */
+/* version: 0.1.18 */
 var Absurd = (function(w) {
 var lib = { 
 	api: {},
@@ -11,8 +11,44 @@ var lib = {
 			helpers: {}
 		},
 		component: { plugins: {}}
-	}
+	},
+	ki: {}
 };
+var queue  = function(funcs, scope) {
+	(function next() {
+		if(funcs.length > 0) {
+			funcs.shift().apply(scope || {}, [next].concat(Array.prototype.slice.call(arguments, 0)));
+		}
+	})();
+}
+var select = function(selector, parent) {
+	var result;
+	try {
+		result = (parent || document).querySelectorAll(selector);
+	} catch(err) {
+		result = document.querySelectorAll(selector);
+	}
+	return result;
+}
+var str2DOMElement = function(html) {
+    var frame = document.createElement('iframe');
+    frame.style.display = 'none';
+    document.body.appendChild(frame);             
+    frame.contentDocument.open();
+    frame.contentDocument.write(html);
+    frame.contentDocument.close();
+    var el = frame.contentDocument.body.firstChild;
+    document.body.removeChild(frame);
+    return el;
+}
+var addEventListener = function(obj, evt, fnc) {
+    if (obj.addEventListener) { // W3C model
+        obj.addEventListener(evt, fnc, false);
+        return true;
+    } else if (obj.attachEvent) { // Microsoft model
+        return obj.attachEvent('on' + evt, fnc);
+    }
+}
 var require = function(v) {
 	// css preprocessor
 	if(v.indexOf('css/CSS.js') > 0) {
@@ -38,158 +74,122 @@ var require = function(v) {
 	}
 };
 var __dirname = "";
-var components = function(absurd) {
-	var api = {},
-		comps = {},
-		cssCache = {},
-		htmlCache = {};
-
-	// utils
-	var select = function(selector) {
-		var result = document.querySelectorAll(selector);
-		if(result.length === 1) { 
-			return result[0]; 
-		} else if(result.length === 0) {
-			return null;
-		} else { 
-			return result; 
-		}
-	}
-	var str2DOMElement = function(html) {
-	    var frame = document.createElement('iframe');
-	    frame.style.display = 'none';
-	    document.body.appendChild(frame);             
-	    frame.contentDocument.open();
-	    frame.contentDocument.write(html);
-	    frame.contentDocument.close();
-	    var el = frame.contentDocument.body.firstChild;
-	    document.body.removeChild(frame);
-	    return el;
-	}
-	var addEventListener = function(obj, evt, fnc) {
-	    if (obj.addEventListener) { // W3C model
-	        obj.addEventListener(evt, fnc, false);
-	        return true;
-	    } else if (obj.attachEvent) { // Microsoft model
-	        return obj.attachEvent('on' + evt, fnc);
-	    }
-	}
-
-	// A base class of the component
-	var Component = function(componentName) {
-		var listeners = [];
-		return {
-			name: componentName,
-			on: function(eventName, callback) {
-				if(!listeners[eventName]) {
-					listeners[eventName] = [];
-				}
-				listeners[eventName].push(callback);
-				return this;
-			},
-			off: function(eventName, handler) {
-				if(!listeners[eventName]) return this;
-				if(!handler) listeners[eventName] = []; return this;
-				var newArr = [];
-				for(var i=0; i<listeners[eventName].length; i++) {
-					if(listeners[eventName][i] !== handler) {
-						newArr.push(listeners[eventName][i]);
-					}
-				}
-				listeners[eventName] = newArr;
-				return this;
-			},
-			dispatch: function(eventName, data) {
-				if(listeners[eventName]) {
-					for(var i=0; i<listeners[eventName].length; i++) {
-						var callback = listeners[eventName][i];
-						callback(data);
-					}
-				}
-				if(this[eventName] && typeof this[eventName] === 'function') {
-					this[eventName](data);
-				}
-				return this;
-			},
-			render: function(selector, data) {
-				var self = this,
-					cssC = cssCache,
-					htmlC = htmlCache;
-
-				var handleCSS = function(css) {
-					if(!cssC[self.name]) {
-						var style = document.createElement("style");
-					    style.setAttribute("id", self.name + '-css');
-					    style.setAttribute("type", "text/css");
-					    style.innerHTML = css;
-						select("head").appendChild(style);
-						cssC[self.name] = { css: css };
-					} else if(cssC[self.name].css !== css) {
-						select('#' + self.name + '-css').innerHTML = css;
-					}
-				}
-
-				var handleHTML = function(html) {
-					if(!htmlC[self.name]) {
-						var el = str2DOMElement(html);
-						select(selector).appendChild(el);
-						htmlC[self.name] = { html: html, element: el };
-					} else if(htmlC[self.name].html !== html) {
-						var newElement = str2DOMElement(html);
-						htmlC[self.name].element.parentNode.replaceChild(newElement, htmlC[self.name].element);
-						htmlC[self.name].element = newElement;
-					}
-					handleDataAttributes(htmlC[self.name].element);
-				}
-
-				var handleDataAttributes = function(element) {
-					var events = element.querySelectorAll('[data-absurd-event]');
-					var handleEventAttribute = function(el) {
-						var value = el.getAttribute("data-absurd-event");
-						if(value !== null) {
-							value = value.split(":");
-							addEventListener(el, value[0], function() {
-								if(typeof self[value[1]] === 'undefined') {
-									throw new Error("There is no method with name '" + value[1] + "'.");
-								} else {
-									self[value[1]].call(self, arguments);
-								}
-							});
-						}
-					}
-					for(var i=0; i<events.length, el=events[i]; i++) {
-						handleEventAttribute(el);
-					}
-					handleEventAttribute(element);
-				}
-
-				// prepare HTML if it is already a DOM element
-				if(this.html && typeof this.html === 'string' && !htmlC[this.name]) {
-					var element = select(this.html);
-					if(element != null) {
-						this.html = element.outerHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-						htmlC[this.name] = {
-							html: this.html,
-							element: element,
-							alreadyAddedDOM: true
-						}
-					}
-				}
-
-				// css & html
-				absurd.flush().morph("component").add(this).compile(function(err, css, html) {
-					css != '' ? handleCSS(css) : null;
-					html != '' ? handleHTML(html) : null;
-				}, api.extend({minify: true}, this));
-
-				self.dispatch("rendered");
+var Observer = function() {
+	var listeners = [];
+	return {
+		on: function(eventName, callback) {
+			if(!listeners[eventName]) {
+				listeners[eventName] = [];
 			}
+			listeners[eventName].push(callback);
+			return this;
+		},
+		off: function(eventName, handler) {
+			if(!listeners[eventName]) return this;
+			if(!handler) listeners[eventName] = []; return this;
+			var newArr = [];
+			for(var i=0; i<listeners[eventName].length; i++) {
+				if(listeners[eventName][i] !== handler) {
+					newArr.push(listeners[eventName][i]);
+				}
+			}
+			listeners[eventName] = newArr;
+			return this;
+		},
+		dispatch: function(eventName, data) {
+			if(listeners[eventName]) {
+				for(var i=0; i<listeners[eventName].length; i++) {
+					var callback = listeners[eventName][i];
+					callback(data);
+				}
+			}
+			if(this[eventName] && typeof this[eventName] === 'function') {
+				this[eventName](data);
+			}
+			return this;
 		}
 	}
+}
+var Component = function(name, absurd) {
+	var CSS = false, HTML = false;
+	var compileCSS = function(next) {
+		if(this.css) {
+			absurd.flush().add(this.css).compile(function(err, css) {
+				if(!CSS) {
+					var style = document.createElement("style");
+				    style.setAttribute("id", name + '-css');
+				    style.setAttribute("type", "text/css");
+				    style.innerHTML = css;
+					(select("head") || select("body"))[0].appendChild(style);
+					CSS = { raw: css, element: style };
+				} else {
+					CSS.raw = css;
+					CSS.element.innerHTML = css;
+				}
+				next();
+			});
+		} else {
+			next();
+		}
+	}
+	var prepareElement = function(next) {
+		if(this.html) {
+			if(HTML === false) {
+				if(typeof this.html === 'string') {
+					var element = select(this.html);
+					if(element.length > 0) {
+						HTML = { el : element[0] }
+						next();
+					} else {
+						next();
+					}
+				} else if(typeof HTML === 'object') {
+					absurd.flush().morph("html").add({'': this.html}).compile(function(err, html) {
+						HTML = { el: str2DOMElement(html) }
+						next();
+					}, this);
+				}
+			} else {
+				next();
+			}
+		} else {
+			next();
+		}
+	}
+	var prepareHTMLString = function(next) {
+		if(HTML && HTML.el) {
+			HTML.raw = HTML.el.outerHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+		}
+		next();
+	}
+	var updateElement = function(next) {
+		if(HTML && HTML.el && HTML.raw) {
+			absurd.flush().morph("html").add({'': HTML.raw}).compile(function(err, html) {
+				// ...
+				next();
+			}, this);
+		}
+		next();
+	}
+	return {
+		populate: function() {
+			queue([
+				compileCSS,
+				prepareElement,
+				prepareHTMLString,
+				updateElement,
+				function() {
+					this.dispatch("populated", {css: CSS, html: HTML});
+				}
+			], this);
+		}
+	}
+}
+var components = function(absurd) {
+	var api = {}, comps = {};
 
-	// components API
-	api.register = function(name, comp) {
-		return comps[name] = api.extend({}, Component(name), comp);
+	api.register = function(name, cls) {
+		return  comps[name] = extend({}, Observer(), Component(name, absurd), cls);
 	}
 	api.get = function(name) {
 		if(comps[name]) { return comps[name]; }
@@ -206,23 +206,15 @@ var components = function(absurd) {
 	}
 	api.flush = function() {
 		comps = {};
+		return api;
 	}
-	api.extend = function() {
-		var process = function(destination, source) {	
-		    for (var key in source) {
-				if (hasOwnProperty.call(source, key)) {
-				    destination[key] = source[key];
-				}
-		    }
-		    return destination;
-		};
-		var result = arguments[0];
-		for(var i=1; i<arguments.length; i++) {
-			result = process(result, arguments[i]);
+	api.broadcast = function(event) {
+		for(var name in comps) {
+			comps[name].dispatch(event);
 		}
-		return result;
+		return api;
 	}
-	
+
 	return api;
 }
 var client = function() {
@@ -485,14 +477,7 @@ lib.api.add = function(API) {
 	}
 	return add;
 }
-var extend = function(destination, source) {
-	for (var key in source) {
-		if (hasOwnProperty.call(source, key)) {
-			destination[key] = source[key];
-		}
-	}
-	return destination;
-};
+var extend = require("../helpers/Extend");
 
 lib.api.compile = function(api) {
 	return function() {
@@ -684,6 +669,21 @@ lib.helpers.ColorLuminance = function (hex, lum) {
 	}
 
 	return rgb;
+}
+lib.helpers.Extend = function() {
+	var process = function(destination, source) {	
+	    for (var key in source) {
+			if (hasOwnProperty.call(source, key)) {
+			    destination[key] = source[key];
+			}
+	    }
+	    return destination;
+	};
+	var result = arguments[0];
+	for(var i=1; i<arguments.length; i++) {
+		result = process(result, arguments[i]);
+	}
+	return result;
 }
 lib.helpers.RequireUncached = function(module) {
 	delete require.cache[require.resolve(module)]
