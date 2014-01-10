650dff90cc80a5942847517a1c0f5c5be93e7105
Krasimir Tsonev <krasimir@outset.ws>
2014-01-10 16:19:52 +0200

    Writing test case for #38

---
+++ tests/bugs/wrong.handling.of.comma.separated.spec.js
@@ -0,0 +1,34 @@
+describe("Wrong handling of Array of Objects in Comma Separated Nested-Selectors", function() {
+
+	var api = require('../../index.js')();
+
+	it("should compile properly", function(done) {
+		// single value
+		api.storage("fontsize", "16px");
+		// multiple values
+		api.storage({
+		    color: "red",
+		    theme: function(color) {
+		        return {
+		            color: color,
+		            background: api.lighten(color, 50)
+		        }
+		    }
+		});
+		api.add({
+		    body: {
+		        background: api.storage("color"),
+		        "section a, section div": [
+		            api.storage("theme")("#999"),
+		            { marginTop: "20px" },
+		            { fontSize: api.storage("fontsize") }
+		        ]
+		    }
+		});
+		api.compile(function(err, css) {
+			expect(css).toBe('body{background: red;}body section a,body section div{background: #e6e6e6;color: #999;margin-top: 20px;font-size: 16px;}');
+			done();
+		}, {minify: true})
+	});
+
+})
