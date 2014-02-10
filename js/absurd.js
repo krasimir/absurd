53e971f6ecf438ba481b5ceeb4a63ff8e1a1ba09
Krasimir Tsonev <krasimir@outset.ws>
2014-02-10 08:57:19 +0200

    Updating version

--- js/absurd.js
+++ js/absurd.js
@@ -1,4 +1,4 @@
-/* version: 0.2.78 */
+/* version: 0.2.80 */
 var Absurd = (function(w) {
 var lib = { 
     api: {},
@@ -434,10 +434,14 @@ api.compileCSS = function(CSS, callback, options) {
 	absurd.flush().add(CSS).compile(callback, options);
 };
 api.qs = function(selector, parent) {
-	return (parent || this.el || document).querySelector(selector);
+	if(parent === false) { parent = document; }
+	else { parent = parent || this.el || document; }
+	return parent.querySelector(selector);
 };
 api.qsa = function(selector, parent) {
-	return (parent || this.el || document).querySelectorAll(selector);
+	if(parent === false) { parent = document; }
+	else { parent = parent || this.el || document; }
+	return parent.querySelectorAll(selector);
 };
 api.getStyle = function(styleProp, el) {
 	el = el || this.el;
@@ -448,6 +452,69 @@ api.getStyle = function(styleProp, el) {
 	}
 	return null;
 };
+api.addClass = function(className, el) {
+	el = el || this.el;
+	if(el.classList) {
+		el.classList.add(className);
+	} else {
+		var current = el.className;
+		if(current.indexOf(className) < 0) {
+			if(current == '') el.className = className;
+			else el.className += ' ' + className;
+		}
+	}
+	return api;
+};
+api.removeClass = function(className, el) {
+	el = el || this.el;
+	if (el.classList) {
+		el.classList.remove(className);
+	} else {
+		var current = el.className.split(' ');
+		var newClasses = [];
+		for(var i=0; i<current.length; i++) {
+			if(current[i] != className) newClasses.push(current[i]);
+		}
+		el.className = newClasses.join(' ');
+	}
+	return api;
+}
+api.replaceClass = function(classNameA, classNameB, el) {
+	el = el || this.el;
+	var current = el.className.split(' '), found = false;
+	for(var i=0; i<current.length; i++) {
+		if(current[i] == classNameA) {
+			found = true;
+			current[i] = classNameB;
+		}
+	}
+	if(!found) {
+		return api.addClass(classNameB, el);
+	}
+	el.className = current.join(' ');
+	return api;
+}
+api.toggleClass = function(className, el) {
+	el = el || this.el;
+	if (el.classList) {
+		el.classList.toggle(className);
+	} else {
+		var classes = el.className.split(' ');
+		var existingIndex = -1;
+		for (var i = classes.length; i--;) {
+	  		if (classes[i] === className)
+	    		existingIndex = i;
+		}
+		
+		if(existingIndex >= 0)
+	  		classes.splice(existingIndex, 1);
+		else
+	  		classes.push(className);
+
+		el.className = classes.join(' ');
+	}
+	return api;
+}
 	return api;
 };
 var client = function() {
@@ -529,8 +596,24 @@ var client = function() {
 				clone = lib.helpers.Clone,
 				comps = {}, 
 				instances = [],
-				events = extend({}, Component());
-			return {
+				events = extend({}, Component()),
+				exports = {};
+
+			(function(fn) {
+				if (document.addEventListener) {
+					document.addEventListener('DOMContentLoaded', fn);
+				} else {
+					document.attachEvent('onreadystatechange', function() {
+						if (document.readyState === 'interactive') {
+							fn();
+						}
+					});
+				}
+			})(function() {
+				exports.broadcast("ready");
+			})
+
+			return exports = {
 				events: events,
 				register: function(name, cls) {
 					return comps[name] = function() {
