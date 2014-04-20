64526b9bca906bfc91039a0179a43b4ec2e41d1d
Daniel Beauchamp <xurix@crimasi.com>
2014-04-20 23:40:51 +0100

    Fixxed tabing issues

--- tests/common/return-processed-external-data.spec.js
+++ tests/common/return-processed-external-data.spec.js
@@ -5,9 +5,9 @@ describe("Should return the processed external data", function() {
 	it("should use the import method", function(done) {
 		var result = api.import(__dirname + '/../data/css/header.js', {minify: true})
 
-        expect(result).toContain('background: #BADA55');
+        	expect(result).toContain('background: #BADA55');
 		expect(result).toContain('height: 97%');
-        done();
+        	done();
 	});
 
 });
